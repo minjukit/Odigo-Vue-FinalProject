8bb2a2fe36646c8af6013e3adb55d9c812f9ce8e
minjukit <minju0426@naver.com>
2023-05-16 15:21:21 +0900

    feat: add BoardDelete

--- src/components/board/BoardDelete.vue
+++ src/components/board/BoardDelete.vue
@@ -17,15 +17,19 @@ import http from "@/util/http-common.js";
 export default {
   name: "BoardDelete",
   created() {
-    http.delete(`/board/${this.$route.params.id}`).then(({ data }) => {
-      let msg = "삭제 처리시 문제가 발생했습니다.";
-      if (data === "success") {
+    http.delete(`/board/${this.$route.params.id}`).then((response) => {
+      let msg =response.status;
+      if (response.status === 200) {
         msg = "삭제가 완료되었습니다.";
       }
       alert(msg);
       // 현재 route를 /list로 변경.
       this.$router.push({ name: "boardList" });
-    });
+    }).catch((err)=>{
+      alert("삭제 처리시 문제가 발생했습니다."+err);
+       this.$router.push({ name: "boardList" });
+    }
+    );
   },
 };
 </script>
