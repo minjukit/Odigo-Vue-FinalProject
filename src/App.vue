3531e1abd2796dc822b23f104bed5a360e8fca36
minju0426 <kimminju04260@gmail.com>
2023-05-25 01:12:53 +0900

    style: add logo & set navbar style

--- src/App.vue
+++ src/App.vue
@@ -38,11 +38,18 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Pretendard-Regular';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+@font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
 }
 
 nav {
